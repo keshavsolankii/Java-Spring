c098195f0c871e12e26ae8b2d5654b3f2e869922
keshavsolankii <keshav26solanki@gmail.com>
2024-02-03 03:24:10 +0530

    Emp file

--- myspring/src/main/java/com/springcore/App.java
+++ myspring/src/main/java/com/springcore/App.java
@@ -15,9 +15,12 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         Student st1 = (Student) context.getBean("student1");
         System.out.println(st1);
-        System.out.println("helosjgogs");
-        System.out.println();
-        System.out.println();
+
+        Student st2 = (Student) context.getBean("student2");
+        System.out.println(st2);
+        
+        Student st3 = (Student) context.getBean("student3");
+        System.out.println(st3);
     }
 
 }
